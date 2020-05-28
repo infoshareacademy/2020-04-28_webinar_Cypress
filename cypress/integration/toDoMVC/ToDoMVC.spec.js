f1a4b2bd5fbf66c844f1bc8cb53c5ed057b5b6c9
maciejkucharski <maciej.kucharski@senti1.com>
2020-05-28 16:49:43 +0200

    prepare to webinar part 2

--- cypress/integration/toDoMVC/ToDoMVC.spec.js
+++ cypress/integration/toDoMVC/ToDoMVC.spec.js
@@ -4,42 +4,40 @@ describe('ToDo App', () => {
   context('When page initially loaded', () => {
     beforeEach(() => {
       cy.visit('');
-    })
+    });
 
     it('header of the page should be visible and have proper name', () => {
       cy.get('.header h1').should('be.visible').and('contain.text', 'todos')
-    })
+    });
 
     it('input should have proper placeholder', () => {
       cy.get('input').should('have.attr', 'placeholder', 'What needs to be done?')
     })
-  })
+  });
 
   context('When added new item', () => {
     beforeEach(() => {
       cy.visit('');
-    })
+    });
 
     it('should be added only one item', () => {
       cy.get('input.new-todo').type('Hello!').type('{enter}')
 
       cy.get('.todo-list li').should('have.length', 1)
-    })
+    });
 
     it('should add 3 items with proper order ', () => {
       const firstItem = "first Item";
       const secondItem = "second Item";
       const thirdItem = "third Item";
 
-      addNewItem(firstItem)
-      addNewItem(secondItem)
-      addNewItem(thirdItem)
+      addNewItem(firstItem);
+      addNewItem(secondItem);
+      addNewItem(thirdItem);
 
       cy.get('.todo-list li').eq(0).should('contain.text', firstItem)
       cy.get('.todo-list li').eq(1).should('contain.text', secondItem)
       cy.get('.todo-list li').eq(2).should('contain.text', thirdItem)
-
-
     })
   })
 });
--- requestApp/script.js
+++ requestApp/script.js
@@ -3,9 +3,9 @@ const saveButton = document.querySelector('#save')
 saveButton.addEventListener('click', () => {
   console.log(4, 'click');
 
-  // const req = new XMLHttpRequest();
-  // req.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
-  // req.send();
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+  req.send();
 
-  fetch("https://jsonplaceholder.typicode.com/users")
+  // fetch("https://jsonplaceholder.typicode.com/users")
 })
